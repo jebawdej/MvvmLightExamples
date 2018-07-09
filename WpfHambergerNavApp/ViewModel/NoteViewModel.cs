b8893e5fa9ca9a8c7907a2a6020c3104e2f8f213
Wim de Jong <wim.dejong@vltest.com>
2018-07-09 09:39:06 +0200

    Adjust Notes Text

--- WpfHambergerNavApp/ViewModel/NoteViewModel.cs
+++ WpfHambergerNavApp/ViewModel/NoteViewModel.cs
@@ -26,7 +26,7 @@ namespace WpfHamburgerNavApp.ViewModel
         }
         public NoteViewModel(IFrameNavigationService fns)
         {
-            NoteText = "Lorem Consectetuer Fermentum&#xD;&#xA;Urna lobortis sodales neque dui iaculis. Libero feugiat feugiat rhoncus ut et platea molestie penatibus. Erat est. Morbi imperdiet. Luctus facilisis faucibus odio nonummy sed nullam placerat purus Curae; nullam nec conubia phasellus, ultrices. Nec quam. Ridiculus taciti duis.&#xD;&#xA;&#xD;&#xA;Eget eu nulla maecenas elementum Semper maecenas nostra, interdum cras nonummy, lorem auctor potenti purus dis metus. Velit varius pede penatibus. Dapibus ipsum ipsum mattis. Dignissim nisl.&#xD;&#xA;&#xD;&#xA;Erat Ac&#xD;&#xA;Bibendum turpis rutrum Commodo. Justo. Placerat hymenaeos elementum pretium est semper. Habitasse nunc amet. Nam. Nulla lectus leo. Donec, pede vitae quisque vivamus porttitor sociis donec accumsan amet. Massa pretium accumsan pretium sociosqu eu.";
+            NoteText = "Lorem Consectetuer Fermentum\r\nUrna lobortis sodales neque dui iaculis. Libero feugiat feugiat rhoncus ut et platea molestie penatibus. Erat est. Morbi imperdiet. Luctus facilisis faucibus odio nonummy sed nullam placerat purus Curae; nullam nec conubia phasellus, ultrices. Nec quam. Ridiculus taciti duis.\r\n\r\nEget eu nulla maecenas elementum Semper maecenas nostra, interdum cras nonummy, lorem auctor potenti purus dis metus. Velit varius pede penatibus. Dapibus ipsum ipsum mattis. Dignissim nisl\r\n\r\nErat Ac\r\nBibendum turpis rutrum Commodo. Justo. Placerat hymenaeos elementum pretium est semper. Habitasse nunc amet. Nam. Nulla lectus leo. Donec, pede vitae quisque vivamus porttitor sociis donec accumsan amet. Massa pretium accumsan pretium sociosqu eu.";
         }
     }
 }
